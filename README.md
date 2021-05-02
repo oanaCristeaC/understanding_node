4bff7dd56f1e26b6bbfe0c6aaea41f28703d621a
Ionela Cristea <oanacristea.1007@yahoo.com>
2021-05-02 13:54:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+file input: `./ex1.mjs --file=text.txt`
+text input from a file: `cat text.txt | ./ex1.mjs --in`
+`getStdin` takes the input stream and pipe it directly to this program
---
+++ ex1.mjs
@@ -0,0 +1,59 @@
+#! /usr/bin/env node
+
+'use strict';
+
+import minimist from 'minimist';
+import fs from 'fs';
+import path from 'path';
+import getStdin from 'get-stdin';
+
+const args = minimist(process.argv.slice(2), {
+  boolean: ['help', 'in'],
+  string: ['file'],
+});
+
+if (args.help) {
+  printHelp();
+} else if (args.in || args._.includes('-')) {
+  getStdin().then(processFile).catch(error);
+} else if (args.file) {
+  fs.readFile(args.file, function (err, contents) {
+    if (err) {
+      error(err.toString());
+    } else {
+      processFile(contents.toString());
+    }
+  });
+} else {
+  error('Incorrect usage', true);
+}
+
+// console.log(args);
+// console.log('p.argv: ', process.argv.slice(2));
+
+function processFile(contents) {
+  process.stdout.write(contents.toUpperCase());
+  // console.log(contents); //buffer
+  // console.log(contents.toString());
+}
+
+function printHelp() {
+  console.log('ex1 usage:');
+  console.log(' ex1.js --file={FILENAME}');
+  console.log('');
+  console.log('--help                                 print this help');
+  console.log('--file={FILENAME}                      process the file');
+  console.log('--in, -                                process stdin');
+  console.log('');
+}
+
+function error(msg, includeHelp = false) {
+  console.error(msg);
+
+  if (includeHelp) {
+    console.log('');
+    printHelp();
+  }
+}
+
+// process.stdout.write('Hello world\n');
